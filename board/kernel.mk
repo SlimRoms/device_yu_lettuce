LineageOS/android_device_yu_lettuce

lettuce: Shift to new device fragment model
* Use make fragments for various subsystems
* Move dexopt flags to device

Change-Id: I2a002f24c308481664b516a761b7978292c3efca

board/kernel.mk
@@ -1,0 +1,2 @@
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_crackling-64_defconfig

SlimRoms/device_yu_lettuce

Conflicts:
        BoardConfig.mk
        device.mk

board/kernel.mk
@@ -1,0 +1,2 @@
+# Kernel
+TARGET_KERNEL_CONFIG := slim_lettuce-64_defconfig